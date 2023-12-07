f0c456591523ff8225a14b01b476707f59ce8852
Tjalf Stadel <tjalf.stadel@web.de>
2023-12-07 09:16:42 +0100

    removed input

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,21 @@
+use std::{fs, path::Path};
+
 fn main() {
-    println!("Hello, world! 2");
+    let input_path = Path::new("input/02.in");
+    let input = fs::read_to_string(input_path).expect("Couldnt open the file");
+    let solution = worker(input);
+    println!("Solution one: {:?}, Solution two: {:?}", solution.0, solution.1);
+}
+
+fn worker(input: String) -> (usize, usize) {
+    let red = 12;
+    let green = 13;
+    let blue = 14;
+    
+    let t: usize = input.lines().filter_map(|a| {
+        let data: Vec<&str> = a.split(": ").collect();
+    }).sum();
+    let p1 = 0;
+    let p2 = 0;
+    (p1, p2)
 }
